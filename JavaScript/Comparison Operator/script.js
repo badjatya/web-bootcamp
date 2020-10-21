692b8d1be04a2e97ad95ea3822c0408b8c75082b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 11:35:29 +0530

    Comparison Operator

---
+++ JavaScript/Comparison Operator/script.js
@@ -0,0 +1,8 @@
+//Comparison Operator
+
+    var score = 99; //variable score declared and assigned value 99
+
+    console.log(score < 50);   //Cheking score less than 50
+    console.log(score <= 99);  //Cheking score less than equal to 99
+    console.log(score > 50);   //Cheking score greater than 50
+    console.log(score >= 99);  //Cheking score greater than equal to 99
