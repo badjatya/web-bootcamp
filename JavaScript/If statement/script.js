362a7080a464cb3c8220bf15819dd4adc5dd66f3
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 12:06:18 +0530

    If Statement

---
+++ JavaScript/If statement/script.js
@@ -0,0 +1,16 @@
+//If Statement
+
+//Syntax 
+    //if (condition) {
+        //Code 
+    // }
+
+//Program
+
+var level = 15; //vriable level is declared and assigned value 15
+
+//if level is greater than equal to 15 than console log will pri
+if (level >= 15) {                                      
+    console.log("level is greater than equal to 15");   
+}
+
