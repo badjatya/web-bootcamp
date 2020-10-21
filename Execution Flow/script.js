ae5f4d8cd0cbb22608da17f36fd94a0b179f5747
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 11:24:00 +0530

    Execution Flow

---
+++ Execution Flow/script.js
@@ -0,0 +1,11 @@
+//Execution Flow
+
+    //JavaScript runs from top to bottom
+
+    var number = 7; //Variable number is declared and assigned a integer value of 7
+
+    number = 9; //Variable number value is updated 
+
+    number = 10; //Variable number value is updated 
+
+    console.log(number); //printing the value of number 
