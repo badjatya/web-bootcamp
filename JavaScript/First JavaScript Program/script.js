860ffbefe93d3f132d3d1b596f7a92cbda95fa76
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 12:30:38 +0530

    First JavaScript Program

---
+++ JavaScript/First JavaScript Program/script.js
@@ -0,0 +1,11 @@
+//First JavaScript Program
+
+var level = 7; //variable declared and assigned
+//if condition
+if (level >= 10){
+    console.log("Can handle ninja's Sword");
+}
+//else condition
+else{
+    console.log("Cant handle ninja's Sword");
+}
