a6ea4daed2890e0d54185b640b678e88d6d921f6
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 12:33:24 +0530

    Functions In JavaScript

---
+++ JavaScript/Functions in JavaScript/script.js
@@ -0,0 +1,14 @@
+//Functions in JavaScript
+
+//Syntax :- 
+
+// function functionName () {
+//     code Block;
+// }
+
+//Program
+    //function hello declared 
+function hello() {
+    console.log("Hello ");
+}
+
