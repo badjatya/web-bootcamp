179a6ffbc33264cb717ec574027f2e0aedd86f23
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 11:16:35 +0530

    console log

---
+++ console log in JavaScript/script.js
@@ -0,0 +1,15 @@
+//console.log() in JavaScript
+
+    //For console press ctrl+shift+i than see console
+
+    //Synatax is console.log();
+
+        //console log with variable
+        var name = "John"; //variable name is defined and John string value is assigned 
+        console.log(name); //in between parenthesis we have used our declared variable
+
+        //console log with string
+        console.log("john"); //This will print string john in lowercase
+
+
+    
