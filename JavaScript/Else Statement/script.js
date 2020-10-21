3bcfd683446d5099ed4552b4b7621aaecceac8d5
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 12:17:20 +0530

    Else Statement

---
+++ JavaScript/Else Statement/script.js
@@ -0,0 +1,12 @@
+//Program
+
+var level = 15; //vriable level is declared and assigned value 15
+
+//if level is greater than equal to 15 than console log will print 
+//if condition is false than else case will run
+if (level >= 15) {                                      
+    console.log("level is greater than equal to 15");   
+}
+else{
+    console.log("False");
+}
