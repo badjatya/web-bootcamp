4968312eefdb144cc2ae92e0893ad5c4ee89938b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-10-21 11:02:12 +0530

    define variables

---
+++ Defining variables in JavaScript/script.js
@@ -0,0 +1,11 @@
+//Defining Variable In JavaScript
+
+    //Syntax: - var variableName = value;
+
+    var name = "John";
+
+
+    //where
+        // var is keyword for declaring variable
+        // name is variable name
+        // John is string in quotes
